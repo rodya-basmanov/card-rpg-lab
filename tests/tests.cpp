e6dbb7e2e0203604d4c8d411dee3d205033790fb
Rodion Basmanov <st135699@student.spbu.ru>
2025-02-19 01:20:25 +0300

    test: add unit tests for new classes and methods

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
 #include "Entity.h"
+#include "Character.h"
+#include "Mage.h"
+#include "Warrior.h"
+#include "Fireball.h"
 
 TEST(EntityTest, HealthAndManaManagement) {
     Entity hero("Hero", 100, 50);
@@ -21,6 +25,50 @@ TEST(EntityTest, HealthAndManaManagement) {
 
     hero.reduceMana(20); 
     EXPECT_EQ(hero.getMana(), 0);
+}
+
+TEST(CharacterTest, Initialization) {
+    Warrior warrior("Warrior", 100, 50, 20, 5);
+
+    EXPECT_EQ(warrior.getName(), "Warrior");
+    EXPECT_EQ(warrior.getHealth(), 100);
+    EXPECT_EQ(warrior.getMana(), 50);
+    EXPECT_EQ(warrior.getAttackPower(), 20);
+    EXPECT_EQ(warrior.getDefense(), 5);
+}
+
+TEST(WarriorTest, AttackMechanics) {
+    Warrior warrior("Warrior", 100, 50, 20, 5);
+    Warrior enemy("Enemy", 100, 50, 20, 5);
+
+    warrior.attack(enemy);
+    EXPECT_EQ(enemy.getHealth(), 85);
+}
+
+TEST(MageTest, FireballAttack) {
+    Mage mage("Mage", 100, 50, 15, 5);
+    Warrior enemy("Enemy", 100, 50, 20, 5);
+
+    mage.attack(enemy);
+    EXPECT_EQ(enemy.getHealth(), 75);
+    EXPECT_EQ(mage.getMana(), 30);
+}
+
+TEST(MageTest, ManaManagement) {
+    Mage mage("Mage", 100, 10, 15, 5);
+    Entity enemy("Enemy", 100, 50);
+
+    mage.attack(enemy);
+    EXPECT_EQ(enemy.getHealth(), 100);
+    EXPECT_EQ(mage.getMana(), 10);
+}
+
+TEST(FireballTest, Activation) {
+    Warrior target("Target", 100, 50, 20, 5);
+    Fireball fireball;
+
+    fireball.activate(target);
+    EXPECT_EQ(target.getHealth(), 75);
 }
 
 int main(int argc, char** argv) {
