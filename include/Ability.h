d8d352f58d2644a2e41bb1c4aef13154f7e15b50
Rodion Basmanov <st135699@student.spbu.ru>
2025-03-16 00:40:38 +0300

    - Adding complete Doxygen documentation for the class and all members - Changing the activate() method to accept both user and target parameters - Adding new virtual methods getManaCost() and applyEffect() for better abstraction - Fixing include dependencies by using forward declaration of Character class - Replacing Character.h with Entity.h header to reduce circular dependencies

--- include/Ability.h
+++ include/Ability.h
@@ -1,21 +1,72 @@
+/**
+ * @file Ability.h
+ * @brief Definition of the Ability abstract class
+ * @details Defines base class for all abilities/skills that characters can use in the game
+ */
 #pragma once
-#include <string>
-#include "Character.h"
+#include "Entity.h"
 
+// Forward declaration
 class Character;
 
+/**
+ * @class Ability
+ * @brief Abstract base class representing character abilities/skills
+ * @details Ability defines the interface for all character abilities that can be
+ * activated against targets and consume mana.
+ */
 class Ability {
 protected:
+    /** @brief Name of the ability */
     std::string name;
+    
+    /** @brief Description of what the ability does */
     std::string description;
 public:
-    Ability(const std::string& name, const std::string& description)
+    /**
+     * @brief Constructor for Ability
+     * @param name The name of the ability
+     * @param description The description of what the ability does
+     */
+    Ability(const std::string & name, const std::string & description)
         : name(name), description(description) {}
 
-    virtual void activate(Character& target) = 0;
+    /**
+     * @brief Activates the ability
+     * @param user The character using the ability
+     * @param target The entity that is targeted by the ability
+     * @details This method handles the ability activation including mana cost
+     * checking, ability application, and any side effects
+     */
+    virtual void activate(Character & user, Entity & target) = 0;
+    
+    /**
+     * @brief Gets the mana cost of the ability
+     * @return The amount of mana required to use this ability
+     */
+    virtual int getManaCost() const = 0;
+    
+    /**
+     * @brief Applies the ability's effect to the target
+     * @param target The entity affected by the ability
+     * @details Implements the primary effect of the ability on the target entity
+     */
+    virtual void applyEffect(Entity& target) = 0;
 
-    const std::string& getName() const { return name; }
-    const std::string& getDescription() const { return description; }
+    /**
+     * @brief Gets the name of the ability
+     * @return The name of the ability
+     */
+    const std::string & getName() const { return name; }
+    
+    /**
+     * @brief Gets the description of the ability
+     * @return The description of the ability
+     */
+    const std::string & getDescription() const { return description; }
 
+    /**
+     * @brief Virtual destructor
+     */
     virtual ~Ability() = default;
 };
